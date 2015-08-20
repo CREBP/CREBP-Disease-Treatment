cb06439fd76eb9923f2c2976b1010769b71c2c7b
Matt Carter <m@ttcarter.com>
2015-08-20 15:28:03 +1000

    Added RCT mapping

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,13 @@ var paths = {
 		csv: './data/sr-disease-interventions.csv',
 		html: './index.html',
 	},
+	rcts: {
+		csv: './data/rct-disease-interventions.csv',
+		html: './index-rcts.html',
+	},
 };
 
-gulp.task('default', ['build:srs']);
+gulp.task('default', ['build:srs', 'build:rcts']);
 
 gulp.task('build:srs', function(next) {
 	async()
@@ -79,6 +83,11 @@ gulp.task('build:srs', function(next) {
 		// Print statistics
 		.then(function(next) {
 			gutil.log(
+				'SR Papers:',
+				_.keys(this.lookup).length
+			);
+
+			gutil.log(
 				'SR Diseases:',
 				_(this.lookup)
 					.pluck('disease')
@@ -89,6 +98,104 @@ gulp.task('build:srs', function(next) {
 
 			gutil.log(
 				'SR Interventions:',
+				_(this.lookup)
+					.pluck('intervention')
+					.uniq()
+					.value()
+					.length
+			);
+
+			next();
+		})
+
+		.end(next);
+});
+
+
+gulp.task('build:rcts', function(next) {
+	async()
+		.limit(50)
+
+		// Read in external data
+		.parallel({
+			rows: function(next) {
+				var rows = [];
+				fs.createReadStream(paths.rcts.csv)
+					.pipe(csvParser())
+					.on('data', function(row) {
+						var values = _.values(row);
+						if (values[0] && values[1] && values[2])
+							rows.push(values);
+					})
+					.on('error', next)
+					.on('finish', function() {
+						next(null, rows);
+					});
+			},
+			html: function(next) {
+				fs.readFile(paths.rcts.html, next);
+			},
+		})
+
+		// Process data into a hash
+		.then('lookup', function(next) {
+			var lookup = {};
+
+			this.rows.forEach(function(row) {
+				var key = row[1] + '-' + row[2];
+				if (lookup[key]) {
+					lookup.value++;
+				} else {
+					lookup[key] = {
+						disease: row[1],
+						intervention: row[2],
+						value: 1,
+					};
+				}
+			});
+			next(null, lookup);
+		})
+
+		// Format back into an array
+		.then('data', function(next) {
+			var data = [];
+			_.forEach(this.lookup, function(relationship) {
+				if (!relationship.disease || !relationship.intervention) return;
+				data.push([
+					relationship.disease,
+					relationship.intervention,
+					relationship.value,
+				]);
+			});
+			next();
+		})
+
+		// Write file
+		.then(function(next) {
+			fs.writeFile(paths.rcts.html, this.html
+				.toString()
+				.replace(/\/\/ AUTO-INSERTED DATA {{{[\s\S]+?}}}/, '// AUTO-INSERTED DATA {{{\n' + util.inspect(this.data) + ';\n// }}}')
+			, next);
+		})
+
+		// Print statistics
+		.then(function(next) {
+			gutil.log(
+				'RCT Papers:',
+				_.keys(this.lookup).length
+			);
+
+			gutil.log(
+				'RCT Diseases:',
+				_(this.lookup)
+					.pluck('disease')
+					.uniq()
+					.value()
+					.length
+			);
+
+			gutil.log(
+				'RCT Interventions:',
 				_(this.lookup)
 					.pluck('intervention')
 					.uniq()

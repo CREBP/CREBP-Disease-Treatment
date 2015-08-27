fb7ffbe5a87e0942ae97c7591c402ebf27d504cc
Matt Carter <m@ttcarter.com>
2015-08-27 11:33:53 +1000

    Added micro server for testing

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+express()
+	.use('/assets', express.static('assets'))
+	.use('/bower_components', express.static('bower_components'))
+	.get('/', function (req, res) {
+		res.sendFile('./index.html', {root: __dirname});
+	})
+	.listen(80, function () {
+		console.log('App listening at http://localhost:80');
+	});
